4e18716a94e633e6723fff77b3bb591fd55ffe57
Alexis Opsasnick <alexiso@umich.edu>
2016-04-03 15:01:26 -0400

    Update transcribeOneNote.m

--- transcribeOneNote.m
+++ transcribeOneNote.m
@@ -2,7 +2,7 @@ function f = transcribeOneNote(YY)
 if(all(YY==0))
 f = 0;
 
-elseif
+else
 AUTO=real(ifft(abs(fft(YY)).^2)); %COMPUTE AUTOCORRELATION OF EACH COLUMN.
 [Q,I]=max(AUTO(55:120,:)); %FIND MAXIMUM (Q) AND ITS LOCATION (I) OF EACH.
 f= 44100./(I+53);
