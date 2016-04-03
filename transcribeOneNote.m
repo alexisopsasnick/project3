cd8c830fd5a6ca664c013eb59a3f9eb61ae94e36
alexisopsasnick <alexiso@umich.edu>
2016-04-03 14:53:02 -0400

    added functinality for rests to the transcriber

--- transcribeOneNote.m
+++ transcribeOneNote.m
@@ -1,5 +1,10 @@
 function f = transcribeOneNote(YY)
+if(all(YY==0))
+f = 0;
+end
+elseif
 AUTO=real(ifft(abs(fft(YY)).^2)); %COMPUTE AUTOCORRELATION OF EACH COLUMN.
 [Q,I]=max(AUTO(55:120,:)); %FIND MAXIMUM (Q) AND ITS LOCATION (I) OF EACH.
 f= 44100./(I+53);
+end
 end
