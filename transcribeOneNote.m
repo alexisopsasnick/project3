fbca5250b1bb845ac60b6235abc8395889cf107f
alexisopsasnick <alexiso@umich.edu>
2016-04-06 16:31:58 -0400

    chooses transcriber based on instrument

--- transcribeOneNote.m
+++ transcribeOneNote.m
@@ -1,23 +1,28 @@
-function f = transcribeOneNote(YY)
+function f = transcribeOneNote(YY, I)
 if(all(YY==0))
 f = 0;
 
 else
+
 %autocorrelation
-AUTO=real(ifft(abs(fft(YY)).^2)); %COMPUTE AUTOCORRELATION OF EACH COLUMN.
-[Q,I]=max(AUTO(55:120,:)); %FIND MAXIMUM (Q) AND ITS LOCATION (I) OF EACH.
-f= 44100./(I+53);
-
-%fudamental frequency identification
-% FYY=abs(fft(YY)); %COMPUTE SPECTRUM OF EACH COLUMN OF YY.
-% [Q,I]=max(FYY(290:580,:)); %FIND MAXIMUM IN THE RANGE FROM 290 to 580.
-% f=(I-1+290)*44100/32768
-
+% AUTO=real(ifft(abs(fft(YY)).^2)); %COMPUTE AUTOCORRELATION OF EACH COLUMN.
+% [Q,I]=max(AUTO(55:120,:)) %FIND MAXIMUM (Q) AND ITS LOCATION (I) OF EACH.
+% f= 44100./(I+53)
+if(I==3)
 %harmonic product spectrum
-% FYY=abs(fft(YY)); %COMPUTE SPECTRUM OF EACH COLUMN OF YY.
-% HPS=FYY(1:800,:).*FYY(1:2:1600,:).*FYY(1:3:2400,:).*FYY(1:4:3200,:).*FYY(1:5:4000,:);
-% [Q,I]=max(HPS(290:580,:)); %FIND MAXIMUM IN THE RANGE FROM 290 to 580.
-% f=(I-1+290)*44100/32768;
+FYY=abs(fft(YY)); %COMPUTE SPECTRUM OF EACH COLUMN OF YY.
+HPS=FYY(1:800,:).*FYY(1:2:1600,:).*FYY(1:3:2400,:).*FYY(1:4:3200,:).*FYY(1:5:4000,:);
+[Q,I]=max(HPS(290:580,:)); %FIND MAXIMUM IN THE RANGE FROM 290 to 580.
+f=(I-1+290)*44100/32768;
+
+else
+%fudamental frequency identification
+FYY=abs(fft(YY)); %COMPUTE SPECTRUM OF EACH COLUMN OF YY.
+[Q,I]=max(FYY(290:580,:)); %FIND MAXIMUM IN THE RANGE FROM 290 to 580.
+f=(I-1+290)*44100/32768
+
+
+end
 
 end
 end
--- transcriber.m
+++ transcriber.m
@@ -1,4 +1,4 @@
-clear;
+
 
 fig=gcf;
 fig.Position= [250 450 650 200];
@@ -25,7 +25,7 @@ else
 note = [note; 'quarter'];
 end
 j=q;
-freqs = [freqs; transcribeOneNote(X, instrument)];
+freqs = [freqs; transcribeOneNote(X, I)];
 
 elseif(XY(i+h) == 0)
 
@@ -33,7 +33,7 @@ X = XY(i:i+h+100-1);
 note = [note; 'half   '];
 
 j=h;
-freqs = [freqs; transcribeOneNote(X, instrument)]; %;0];
+freqs = [freqs; transcribeOneNote(X, I)]; %;0];
 
 elseif(XY(i+w) ==0)
 
@@ -42,7 +42,7 @@ X = XY(i:i+w+100-1);
 note = [note; 'whole  '];
 
 j=w;
-freqs = [freqs; transcribeOneNote(X, instrument)]; %; 0; 0; 0];
+freqs = [freqs; transcribeOneNote(X, I)]; %; 0; 0; 0];
 
 end
 
